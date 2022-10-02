d342d2f0a3b369556e914e2fec94cc8be0438a62
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-02 16:49:48 -0300

    Actualizar el listado de carros

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -39,7 +39,7 @@ export class CarsController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCarDto: UpdateCarDto,
   ) {
-    return updateCarDto;
+    return this.carsService.update(id, updateCarDto);
   }
 
   @Delete(':id')
