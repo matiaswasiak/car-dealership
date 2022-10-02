64ed9b47b2d7eda11e877c62e1e1f4a5b06c9158
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-02 16:39:38 -0300

    Actualizar un carro

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -13,6 +13,7 @@ import {
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -34,8 +35,11 @@ export class CarsController {
   }
 
   @Patch(':id')
-  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
-    return body;
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto,
+  ) {
+    return updateCarDto;
   }
 
   @Delete(':id')
---
+++ src/cars/dto/update-car.dto.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { IsString, IsUUID, IsOptional } from 'class-validator';
+
+export class UpdateCarDto {
+  @IsString()
+  @IsUUID()
+  @IsOptional()
+  readonly id?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly brand?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly model?: string;
+}
